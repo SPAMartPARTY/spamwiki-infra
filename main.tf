d2d446aa5ed75fcb6ad5ec2caf45b29773321092
SPAMartPARTY <spamartparty@proton.me>
2025-06-03 07:25:01 -0600

    Fix formatting to pass terraform fmt check

--- main.tf
+++ main.tf
@@ -12,30 +12,30 @@ provider "libvirt" {
 }
 
 module "meatartbot_vm" {
-  source     = "./modules/vm"
-  name       = "meatartbot"
-  memory     = 4096
-  vcpus      = 2
-  image_url  = var.image_url
-  pool       = var.pool
-  network    = var.network
+  source             = "./modules/vm"
+  name               = "meatartbot"
+  memory             = 4096
+  vcpus              = 2
+  image_url          = var.image_url
+  pool               = var.pool
+  network            = var.network
   ssh_authorized_key = file("~/.ssh/meatartbot.pub")
   #ssh_key  = file("~/.ssh/meatartbot.pub")
-  mac_address = "52:54:00:c6:38:81" 
+  mac_address = "52:54:00:c6:38:81"
   #pool_path  = "/home/perfesser/libvirt-images"
 
 }
 
 # Repeat for other VMs:
 module "ollama_vm" {
-  source     = "./modules/vm"
-  name       = "ollama"
-  memory     = 8192
-  vcpus      = 4
-  image_url  = var.image_url
-  pool       = var.pool
-  network    = var.network
-  mac_address = "52:54:00:86:74:ec"
+  source             = "./modules/vm"
+  name               = "ollama"
+  memory             = 8192
+  vcpus              = 4
+  image_url          = var.image_url
+  pool               = var.pool
+  network            = var.network
+  mac_address        = "52:54:00:86:74:ec"
   ssh_authorized_key = file("~/.ssh/meatartbot.pub")
   #ssh_key  = file("~/.ssh/meatartbot.pub")
   #pool_path  = "/home/perfesser/libvirt-images"
