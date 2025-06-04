569915a80918e6a51a35c72828fa5ecb9244cf8a
SPAMartPARTY <spamartparty@proton.me>
2025-06-03 20:25:19 -0600

    Fix terraform formatting

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ provider "libvirt" {
 }
 
 module "meatartbot_vm" {
-  source             = "./modules/vm"
-  name               = "meatartbot"
-  memory             = 4096
-  vcpus              = 2
-  image_url          = var.image_url
-  pool               = var.pool
-  network            = var.network
+  source    = "./modules/vm"
+  name      = "meatartbot"
+  memory    = 4096
+  vcpus     = 2
+  image_url = var.image_url
+  pool      = var.pool
+  network   = var.network
   #ssh_authorized_key = file("~/.ssh/meatartbot.pub")
   ssh_authorized_key = file("${path.module}/meatartbot.pub")
   #ssh_key  = file("~/.ssh/meatartbot.pub")
@@ -29,14 +29,14 @@ module "meatartbot_vm" {
 
 # Repeat for other VMs:
 module "ollama_vm" {
-  source             = "./modules/vm"
-  name               = "ollama"
-  memory             = 8192
-  vcpus              = 4
-  image_url          = var.image_url
-  pool               = var.pool
-  network            = var.network
-  mac_address        = "52:54:00:86:74:ec"
+  source      = "./modules/vm"
+  name        = "ollama"
+  memory      = 8192
+  vcpus       = 4
+  image_url   = var.image_url
+  pool        = var.pool
+  network     = var.network
+  mac_address = "52:54:00:86:74:ec"
   #ssh_authorized_key = file("~/.ssh/meatartbot.pub")
   ssh_authorized_key = file("${path.module}/meatartbot.pub")
   #ssh_key  = file("~/.ssh/meatartbot.pub")
